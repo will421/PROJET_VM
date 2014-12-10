2b0f6c97d5169b65b9f08c429d2cb4e71d0531d4
will421 <willou.421@gmail.com>
2014-12-10 10:59:16 +0100

    dependancy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='projet_VM',
+      version='0.1',
+      url='https://github.com/will421/PROJET_VM',
+      author='William Bobo',
+      license='MIT',
+      install_requires=[
+          'lxml',
+		  'pykml',
+      ],
+      zip_safe=False)
